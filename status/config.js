4073ad778510f352339248b24154c8426ea3a6b2
zigou23 <a1923458037@gmail.com>
2022-04-13 14:16:32 +0800

    status change

--- status/config.js
+++ status/config.js
@@ -7,11 +7,11 @@ window.Config = {
   // 站点链接
   SiteUrl: '/status/',
 
-  // UptimeRobot Api 域名
+  // UptimeRobot Api 域名 api.uptimerobot.com
   // 只需填写域名部分，默认为官网域名
   // 因官网 API 时不时的会 CROS 报错，可填自定义反代域名
   // 详见 https://github.com/yb/uptime-status/ 说明
-  ApiDomain: 'api.uptimerobot.com',
+  ApiDomain: 'qsim-status.zigou.workers.dev',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
