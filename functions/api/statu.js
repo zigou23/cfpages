68788ff58299f1d41a907da94fe9df6669d7cf78
zigou <40587032+zigou23@users.noreply.github.com>
2023-02-22 11:32:24 +0800

    Update statu.js

--- functions/api/statu.js
+++ functions/api/statu.js
@@ -17,14 +17,14 @@ export async function onRequest(context) {
   // if (!allowedOrigins.includes(origin)) {
   //   return new Response('Forbidden', { status: 403 });
   // }
-  
+
   let url = new URL(request.url);
   let response = await fetch('https://api.uptimerobot.com/v2/getMonitors', request);
   response = new Response(response.body, response);
   
   // 这个填写需要的域名，比如('Access-Control-Allow-Origin', 'https://lab.qsim.top')，跨域会访问访问不了，因为我是同域名访问，所以不需要
   // response.headers.set('Access-Control-Allow-Origin', '*');
-  response.headers.set('Access-Control-Allow-Methods', '*');
+  response.headers.set('Access-Control-Allow-Methods', 'POST');
   response.headers.set('Access-Control-Allow-Credentials', 'true');
   response.headers.set('Access-Control-Allow-Origin', 'https://qsim.top');
   response.headers.set('Access-Control-Allow-Headers', 'Content-Type,Access-Token');
