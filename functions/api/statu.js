ddcf4f1fe2ae3437f808c9bb5f6271136aa00ea0
zigou <40587032+zigou23@users.noreply.github.com>
2023-02-22 11:24:35 +0800

    Update statu.js

--- functions/api/statu.js
+++ functions/api/statu.js
@@ -1,5 +1,12 @@
 export async function onRequest(context) {
-  const { request } = context;
+  const {
+      request, // same as existing Worker API
+      env, // same as existing Worker API
+      params, // if filename includes [id] or [[path]]
+      waitUntil, // same as ctx.waitUntil in existing Worker API
+      next, // used for middleware or to fetch assets
+      data, // arbitrary space for passing data between middlewares
+  } = context;
   
   // 允许访问的域名列表
   const allowedOrigins = ['https://qsim.top'];
